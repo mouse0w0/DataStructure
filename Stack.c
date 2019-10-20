05a563f4d982fe068207fd60f84c2fe2d30ab7c8
Mouse <mouse0w0@qq.com>
2019-10-20 20:41:14 +0800

    update Stack

--- Stack.c
+++ Stack.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 100
-
 typedef enum {
     false = 0, true = 1
 } bool;
 typedef int ElementType;
 typedef struct {
-    ElementType array[MAX_SIZE];
+    ElementType *array;
+    int size;
     int top;
 } Stack;
 typedef Stack *PStack;
 
-PStack InitStack();
+PStack InitStack(int size);
 
 bool IsEmpty(PStack stack);
+
+bool IsFull(PStack stack);
 
 bool Push(PStack stack, ElementType value);
 
@@ -25,8 +26,10 @@ int main() {
     return 0;
 }
 
-PStack InitStack() {
+PStack InitStack(int size) {
     PStack stack = malloc(sizeof(Stack));
+    stack->size = size;
+    stack->array = malloc(size * sizeof(ElementType));
     stack->top = -1;
     return stack;
 }
@@ -35,8 +38,12 @@ bool IsEmpty(PStack stack) {
     return stack->top == -1;
 }
 
+bool IsFull(PStack stack) {
+    return stack->top == stack->size - 1;
+}
+
 bool Push(PStack stack, ElementType value) {
-    if (stack->top == MAX_SIZE - 1) {
+    if (IsFull(stack)) {
         return false;
     }
 
