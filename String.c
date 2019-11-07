7bf749e47ff57b235b3bef8568239722cd09f1c8
Mouse <mouse0w0@qq.com>
2019-11-07 20:14:54 +0800

    Update String

--- String.c
+++ String.c
@@ -1,3 +1,63 @@
+#include <stdlib.h>
 #include "includes/String.h"
 
+String StrCreate(char *value) {
+    String s;
+    StrAssign(&s, value);
+    return s;
+}
 
+int StrLength(String value) {
+    int length = 0;
+    while (*(value++)) length++;
+    return length;
+}
+
+void StrAssign(StringRef ref, String value) {
+    String s = *ref = malloc((StrLength(value) + 1) * sizeof(char));
+    while (*value) *(s++) = *(value++);
+    *s = '\0';
+}
+
+void StrConcat(StringRef ref, String s2) {
+    String s1 = *ref;
+    String s = *ref = malloc((StrLength(s) + StrLength(s2) + 1) * sizeof(char));
+    while (*s1) *(s++) = *(s1++);
+    while (*s2) *(s++) = *(s2++);
+    *s = '\0';
+}
+
+String SubStr(String s, int start, int length) {
+    String result = malloc((length + 1) * sizeof(char));
+    int i;
+    for (i = 0; i < length && s[start + i] != '\0'; i++) {
+        result[i] = s[start + i];
+    }
+    result[length] = '\0';
+    return result;
+}
+
+int StrCmp(String s1, String s2) {
+    while (*s1 && *s2) {
+        char c1 = *(s1++), c2 = *(s2++);
+        if (c1 > c2) return 1;
+        else if (c1 < c2) return -1;
+    }
+    return 0;
+}
+
+int StrIndex(String s, String t) {
+
+}
+
+void StrInsert(StringRef ref, int i, String t) {
+
+}
+
+void StrDelete(StringRef ref, int start, int length) {
+
+}
+
+void StrRep(StringRef ref, String t, String r) {
+
+}
--- includes/String.h
+++ includes/String.h
@@ -2,12 +2,15 @@
 #define DATASTRUCTURE_STRING_H
 
 typedef char *String;
+typedef String *StringRef;
+
+String StrCreate(char *value);
 
 int StrLength(String value);
 
-void StrAssign(String s, String value);
+void StrAssign(StringRef s, String value);
 
-void StrConcat(String s1, String s2, String dest);
+void StrConcat(StringRef s1, String s2);
 
 String SubStr(String s, int start, int length);
 
@@ -15,11 +18,11 @@ int StrCmp(String s1, String s2);
 
 int StrIndex(String s, String t);
 
-void StrInsert(String s, int i, String t);
+void StrInsert(StringRef s, int i, String t);
 
-void StrDelete(String s, int start, int length);
+void StrDelete(StringRef s, int start, int length);
 
-void StrRep(String s, String t, String r);
+void StrRep(StringRef s, String t, String r);
 
 #endif //DATASTRUCTURE_STRING_H
 
